93adfab8a1c7e467b4a67b4133997d3479040469
anaesparapani <159201364+anaesparapani@users.noreply.github.com>
2024-07-31 11:27:16 -0300

    Add files via upload

---
+++ comparacao_de_notas.js
@@ -0,0 +1,10 @@
+const nota = 8;
+if(nota<4){
+    console.log("O aluno esta reprovado")
+}
+else if(nota>4&5){
+    console.log("O aluno esta de recuperacao")
+}
+else{
+    console.log("O aluno esta aprovado")
+}
