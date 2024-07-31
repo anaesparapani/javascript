c923a7d6f5673b902bd836b0bfad90136ceddd64
anaesparapani <159201364+anaesparapani@users.noreply.github.com>
2024-07-31 13:59:45 -0300

    Add files via upload

---
+++ classificacao_de_idade.js
@@ -0,0 +1,16 @@
+const idade = 16;
+if(idade<0){
+    console.log("Erro numero negativo, digite um positivo")
+}
+else if(idade <=12){
+    console.log("Criança")
+}
+else if(idade<=17){
+    console.log("Adolescente")
+}
+else if(idade<=64){
+    console.log("Adulto")
+}
+else{
+    console.log("Idoso")
+}
